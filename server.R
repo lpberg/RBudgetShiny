e73b4e8f773fdc00dc0aaaa21327fdcdbb2a9506
Berg <lpberg@srn.sandia.gov>
2019-01-20 10:34:00 -0700

    updated for use with MINT

--- server.R
+++ server.R
@@ -7,34 +7,37 @@ library(plotly)
 library(lubridate)
 
 #read in transactions from excel
-all_transactions<- read_excel("ExportedTransactions.xls")
-names(all_transactions)
-all_transactions$Description = str_replace_all(all_transactions$Description, "\\s+", " ")
+all_transactions<- read_csv("transactions.csv")
+
+# all_transactions$Description = str_replace_all(all_transactions$Description, "\\s+", " ")
 
 #read in dictionary from excel
-dict <- read_excel("dictionary.xls")
-dict$Description = str_replace_all(dict$Description, "\\s+", " ")
+# dict <- read_excel("dictionary.xls")
+# dict$Description = str_replace_all(dict$Description, "\\s+", " ")
 #combine transaction table with dictionary table to apply labels
-all_trans_with_labels = left_join(all_transactions, dict, by="Description")
-
-blanks <- all_trans_with_labels[is.na(all_trans_with_labels$specific_catagory),]
-if(nrows(blanks) > 0) {
-  write_excel_csv(blanks,path = "blanks.csv")
-}
+# all_transactions = left_join(all_transactions, dict, by="Description")
+# 
+# blanks <- all_transactions[is.na(all_transactions$specific_catagory),]
+# if(nrows(blanks) > 0) {
+#   write_excel_csv(blanks,path = "blanks.csv")
+# }
+summary(all_transactions)
 #reformat posting date column
-all_trans_with_labels$`Posting Date` <- as.Date(all_trans_with_labels$`Posting Date` , "%m/%d/%y",  tz = "CST6CDT")
-all_trans_with_labels <- rename(all_trans_with_labels, 
-                                `posting_date` = `Posting Date`,
-                                `transaction_type` = `Transaction Type`,
-                                `amount` = `Amount`,
-                                `interest` = `Interest`,
-                                `check_num` = `Check Number`,
+all_transactions$Date <- mdy(all_transactions$Date)
+all_transactions$`Account Name` <- as.character(all_transactions$`Account Name`)
+all_transactions <- rename(all_transactions, 
+                                `posting_date` = `Date`,
                                 `description` = `Description`,
-                                `transaction_cat` = `Transaction Category`,
-                                `balance` = `Balance`)
+                                `orig_description` = `Original Description`,
+                                `amount` = `Amount`,
+                                `transaction_type` = `Transaction Type`,
+                                `acount_name` = `Account Name`,
+                                `transaction_cat` = `Category`)
 
 #set amounts to be inverse (not negative)
-all_trans_with_labels$amount <- all_trans_with_labels$amount / -1
+# all_transactions$amount <- all_transactions$amount / -1
+
+summary(all_transactions)
 
 server <- function(input, output) {
   #UI element for setting the amount range to display
@@ -49,19 +52,19 @@ server <- function(input, output) {
   #UI element for selecting categories
   output$categories = renderUI({
     selectInput("categories", "Select Categories:", 
-                unique(c(c(all_trans_with_labels$specific_catagory),c(all_trans_with_labels$generic_catagory))),
-                multiple = TRUE, selected = c('amazon'))
+                unique(c(c(all_transactions$description),c(all_transactions$transaction_cat))),
+                multiple = TRUE, selected = c('Target'))
   })
   #UI element for setting date range of transactions
   output$dateRange = renderUI({
     dateRangeInput('dateRange',
          label = 'Date range', 
          format = 'MM-yy',startview = "week",
-         start = min(all_trans_with_labels$posting_date), 
-         end = max(all_trans_with_labels$posting_date))
+         start = min(all_transactions$posting_date), 
+         end = max(all_transactions$posting_date))
   })
   aggregateByMonth <- reactive({
-    df = getFilteredData() %>% group_by(lab1 = `specific_catagory`, 
+    df = getFilteredData() %>% group_by(lab1 = `description`, 
         month = paste(
           months(floor_date(`posting_date`,"month")),
            year(floor_date(`posting_date`,"month")))) %>%
@@ -69,21 +72,21 @@ server <- function(input, output) {
     return(df)
   })
   getFilteredData <- reactive({
-    filtered_df = all_trans_with_labels %>%
-      select(c(`amount`,`specific_catagory`,`generic_catagory`,`posting_date`)) %>%
+    filtered_df = all_transactions %>%
+       select(c(`amount`,`description`,`transaction_cat`,`posting_date`)) %>%
       filter(`posting_date` >= input$dateRange[1] & `posting_date` <= input$dateRange[2]) %>%
       filter(`amount` >= input$valRange[1] & `amount` <= input$valRange[2])
     #only show labels from input$labels (show all is default) 
     if (length(input$categories) != 0){
       filtered_df = filtered_df %>%
-        filter(`specific_catagory` %in% input$categories | `generic_catagory` %in% input$categories)
+        filter(`description` %in% input$categories | `transaction_cat` %in% input$categories)
     }
     return(filtered_df)
   })
   
   output$plot1 <- renderPlotly({
     df = getFilteredData()
-    p <- plot_ly(data = df, x = ~`posting_date`, y = ~`amount`, color = ~`generic_catagory`)
+    p <- plot_ly(data = df, x = ~`posting_date`, y = ~`amount`, color = ~`transaction_cat`)
     return(p)
   })
   
