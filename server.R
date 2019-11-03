995b1d18bcb0bfdd3e7d38ebf39350c8ed74497f
Leif Berg <leif.berg@gmail.com>
2019-11-03 16:13:43 -0600

    updated gitignore, added shinydashboard, updated transaction file

--- server.R
+++ server.R
@@ -5,36 +5,40 @@ library(shiny)
 library(DT)
 library(plotly)
 library(lubridate)
+library(shinydashboard)
 source(file.path('helperFunctions.R', fsep = .Platform$file.sep))
+
 #read in transactions from csv
 all_transactions <- readInTransactions("transactions.csv")
-#set amounts to be inverse (not negative)
-# all_transactions$amount <- all_transactions$amount / -1
-
-summary(all_transactions)
 
 server <- function(input, output) {
+    
   #UI element for selecting categories
   output$accounts = renderUI({
     selectInput("accounts", "Accounts:", 
                 unique(all_transactions$account_name),
-                multiple = TRUE, selected = c('S/D - Money Manager'))
+                multiple = FALSE, 
+                selected = c("Fidelity Rewards Visa Signature"))
   })
+  
   #UI element for setting the amount range to display
   output$valRange = renderUI({
-    sliderInput("valRange", "Amount Range", 
+    sliderInput("valRange", 
+                "Amount Range", 
                 min = 0, 
                 max = 2000, 
                 value=c(1,2000), 
                 step = NULL, 
                 round = TRUE)
   })
+  
   #UI element for selecting categories
   output$categories = renderUI({
     selectInput("categories", "Categories:", 
                 unique(c(c(all_transactions$description),c(all_transactions$transaction_cat))),
                 multiple = TRUE, selected = c('Target'))
   })
+  
   #UI element for setting date range of transactions
   output$dateRange = renderUI({
     dateRangeInput('dateRange',
@@ -43,6 +47,7 @@ server <- function(input, output) {
          start = min(all_transactions$posting_date), 
          end = max(all_transactions$posting_date))
   })
+  
   aggregateByMonth <- reactive({
     df = getFilteredData() %>% group_by(lab1 = `description`, 
         month = paste(
@@ -51,6 +56,7 @@ server <- function(input, output) {
       summarize(amount = sum(`amount`))
     return(df)
   })
+  
   getFilteredData <- reactive({
     filtered_df = all_transactions %>%
        select(c(`amount`,`description`,`transaction_cat`,`posting_date`,`account_name`)) %>%
@@ -67,7 +73,12 @@ server <- function(input, output) {
   
   output$plot1 <- renderPlotly({
     df = getFilteredData()
-    p <- plot_ly(data = df, x = ~`posting_date`, y = ~`amount`, color = ~`transaction_cat`)
+    p <- plot_ly(data = df, 
+                 type = 'scatter', 
+                 size =  ~`amount`,
+                 x = ~`posting_date`, 
+                 y = ~`amount`, 
+                 color = ~`transaction_cat`)
     return(p)
   })
   
@@ -82,19 +93,20 @@ server <- function(input, output) {
     return(p)
   })
   
-   output$table <- renderUI({
-     output$tt <- DT::renderDataTable(DT::datatable(getFilteredData(), rownames = FALSE, options = list( #disabling rowname prevents the index from being displayed
-       pageLength = 50
-     )))
-     dataTableOutput('tt')
+   output$transaction_list <- DT::renderDataTable({
+      # validate(
+      #  need(!is.null(getFilteredData()))
+      # )
+      df <- getFilteredData()
+      DT::datatable(df,
+                    rownames = FALSE,
+                    options = list(pageLength = 50))
    })
    
-   output$table2 <- renderUI({
-     output$tt2 <- DT::renderDataTable(DT::datatable(aggregateByMonth(), rownames = FALSE, options = list( #disabling rowname prevents the index from being displayed
-       pageLength = 50
-     )))
-     dataTableOutput('tt2')
-   })
+   output$monthly_summary_list <- DT::renderDataTable({
+     DT::datatable(aggregateByMonth(),
+                                       rownames = FALSE,
+                                       options = list(pageLength = 50))   })
    
 }
 
--- ui.R
+++ ui.R
@@ -1,26 +1,51 @@
 library(plotly)
 library(shiny)
+library(DT)
 
-ui <- fluidPage(
-  titlePanel("budget"),
-  sidebarLayout(
-    sidebarPanel(width=4,
-                 uiOutput("accounts"),
-                 uiOutput("valRange"),
-                 uiOutput("categories"),
-                 uiOutput("dateRange")
-    ),
-    mainPanel(width = 8,
-              tabsetPanel(type="tabs",
-                          tabPanel("Scatter Plot",  
-                                   plotlyOutput("plot1"),
-                                   uiOutput("table")
-                          ),
-                          tabPanel("Monthly Summary",
-                                   plotlyOutput("plot2"),
-                                   uiOutput("table2")
-                          )
-              )
+library(shinydashboard)
+
+ui <- dashboardPage(title = "Budget Breakdown",
+    dashboardHeader(),
+    dashboardSidebar(uiOutput("accounts"),
+                     uiOutput("valRange"),
+                     uiOutput("categories"),
+                     uiOutput("dateRange")),
+    dashboardBody(
+        
+        tabsetPanel(
+            type="tabs",
+            tabPanel("Scatter Plot",  
+                     plotlyOutput("plot1"),
+                     DTOutput("transaction_list")
+            ),
+            tabPanel("Monthly Summary",
+                     plotlyOutput("plot2"),
+                     DTOutput("monthly_summary_list")
+            )
+        )
+        
     )
-  )
 )
+# 
+# 
+# ui <- fluidPage(
+#   titlePanel("budget"),
+#   sidebarLayout(
+#     sidebarPanel(width=4,
+#                  
+#     ),
+#     mainPanel(width = 8,
+#               tabsetPanel(
+#                   type="tabs",
+#                   tabPanel("Scatter Plot",  
+#                          plotlyOutput("plot1"),
+#                          DTOutput("transaction_list")
+#                   ),
+#                   tabPanel("Monthly Summary",
+#                           plotlyOutput("plot2"),
+#                           DTOutput("monthly_summary_list")
+#                   )
+#               )
+#     )
+#   )
+# )
